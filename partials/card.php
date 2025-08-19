65c52912f1dc66977cb6de1a883d39047dd7be10
glenmark <coddybanks178@gmail.com>
2025-08-19 15:21:18 +0800

    main page feature header,footer,home and index

---
+++ partials/card.php
@@ -0,0 +1,9 @@
+<div class="card mb-3">
+  <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Sample Item</h5>
+    <p class="card-text">Delicious food description.</p>
+    <a href="#" class="btn btn-primary">Order Now</a>
+  </div>
+</div>
+

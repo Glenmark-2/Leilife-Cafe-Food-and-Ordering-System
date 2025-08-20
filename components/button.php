489b323792bffccbee6cbf211e9629d4dabd61c3
Nico <ncflrs07@gmail.com>
2025-08-20 23:22:00 +0800

    forgot password/modify button,menu-card,signup

--- components/button.php
+++ components/button.php
@@ -1,21 +1,42 @@
 <style> .category-buttons {
+   
     display: flex;       /* arrange children in a row */
     gap: 10px;           /* space between buttons */
 }
 
-.category-buttons button {
+.card-button {
+    margin-top: 10px;
+}
+.card-button button {
+    
     padding: 10px 20px;
     border: none;
-    background: #333;
+    background: #22333B;
     color: white;
     cursor: pointer;
     border-radius: 50px;
+    transition: 0.3s ease;
 }
+.category-buttons button {
+    width: 120px;
+    height: 30px;
+    padding: 10px 20px;
+    border: none;
+    background: #5E503F;
+    color: white;
+    cursor: pointer;
+    border-radius: 50px;
+    transition: 0.3s ease;
+}
+
+
+
+
 </style>
 <?php
 
 echo '
-
-      <button class="card-button">'.$Text.'</button>
+    
+      <button class=".$class">'.$Text.'</button>
     
 ';
---
+++ pages/forgot-password.php
@@ -0,0 +1,17 @@
+<style>
+    .password-field{
+        text-align: center;
+    }
+</style>
+<div class="password-field">
+    <h1>Create a new password</h1>
+    <p>Must be at-least 8 characters.</p>
+    <div class="fill-up-form">
+        <h3>Enter a new password</h3>
+        <form>
+  <input type="text" id="fname" name="fname" placeholder="Enter a new password"><br>
+  <input type="text" id="lname" name="lname" placeholder ="Confirm password"><br>
+</form>
+
+    </div>
+</div>

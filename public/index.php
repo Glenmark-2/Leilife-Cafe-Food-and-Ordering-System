635100534112e938c8be0581b4aff69bf493b615
Ennovie Evangelista <ennovieevangelista@Ennovies-MacBook-Air.local>
2025-08-25 12:26:20 +0800

    new page added, user-profile

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ $page = $_GET['page'] ?? 'home';
 
 include '../components/header.php';
 
-$allowed_pages = ['home', 'menu', 'orders','signUp','login','solo-product'];
+$allowed_pages = ['home', 'menu', 'orders','signUp','login','solo-product','user-profile'];
 if(in_array($page, $allowed_pages)){
     include "../pages/$page.php";
 } else {

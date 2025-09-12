067c33aeb02c4cd05fa55989ee79c3bab8955d37
Nico <ncflrs07@gmail.com>
2025-09-12 11:06:15 +0800

    modal added to admin roles

--- backend/db_script/db.php
+++ backend/db_script/db.php
@@ -8,7 +8,7 @@ try {
     $pdo = new PDO(
         "mysql:host=" . $_ENV['DB_HOST'] . ";dbname=" . $_ENV['DB_NAME'],
         $_ENV['DB_USER'],
-        $_ENV['DB_PASS']
+        // $_ENV['DB_PASS']
     );
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {

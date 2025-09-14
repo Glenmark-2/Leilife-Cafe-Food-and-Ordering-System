49fb4c8a4a8ffe745506a32dc2e4400e8a8d83d3
Ennovie Evangelista <ennovieevangelista@Ennovies-MacBook-Air.local>
2025-09-14 15:51:24 +0800

    cart unfinished

--- backend/db_script/env.php
+++ backend/db_script/env.php
@@ -1,20 +1,21 @@
 <?php
-function loadEnv($path) {
-    if (!file_exists($path)) {
-        throw new Exception(".env file not found at $path");
-    }
-
-    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (str_starts_with(trim($line), '#')) {
-            continue; // skip comments
+if (!function_exists('loadEnv')) {
+    function loadEnv($path) {
+        if (!file_exists($path)) {
+            throw new Exception(".env file not found at $path");
         }
-        list($name, $value) = explode('=', $line, 2);
-        $name = trim($name);
-        $value = trim($value);
 
-        // set in PHP superglobals
-        $_ENV[$name] = $value;
-        putenv("$name=$value");
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (str_starts_with(trim($line), '#')) {
+                continue; // skip comments
+            }
+            list($name, $value) = explode('=', $line, 2);
+            $name = trim($name);
+            $value = trim($value);
+
+            $_ENV[$name] = $value;
+            putenv("$name=$value");
+        }
     }
 }

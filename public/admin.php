621ac3a8f6de621a4724866947c527b9a713ffd9
Ennovie Evangelista <ennovie23@gmail.com>
2025-10-10 17:07:57 +0800

    sales report in pdf not done

--- public/admin.php
+++ public/admin.php
@@ -10,7 +10,7 @@ $page = $currentPage;
 
     <div id="content-wrapper">
         <?php
-        $allowed_pages = ['dashboard', 'sales', 'products', 'roles', 'inbox','reports','audit'];
+        $allowed_pages = ['dashboard', 'sales', 'products', 'roles', 'inbox','reports','audit','sales-report-pdf'];
         if (in_array($currentPage, $allowed_pages)) {
             include"../pages/admin/$currentPage.php";
         } else {
